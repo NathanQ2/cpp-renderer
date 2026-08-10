d6808ad1914657e9d5ae630d4af240272331a8ac
Nathan Q <Natethegreat08q@gmail.com>
2026-08-10 12:47:21 -0400

    Add PT_VERIFY() and PT_CORE_VERIFY() macros

--- PalmTree/src/PalmTree/Logging/Log.h
+++ PalmTree/src/PalmTree/Logging/Log.h
@@ -25,11 +25,22 @@ namespace PalmTree {
 #define PT_WARN(...)  ::PalmTree::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define PT_ERROR(...) ::PalmTree::Log::GetClientLogger()->error(__VA_ARGS__)
 
+#if defined(_MSC_VER)
+    #include <intrin.h>
+    #define DEBUG_BREAK() __debugbreak()
+#elif defined(__clang__) || defined(__GNUC__)
+    #define DEBUG_BREAK() __builtin_trap()
+#else
+    #define DEBUG_BREAK() std::abort()
+#endif
+
 #ifdef PT_ENABLE_ASSERTS
-    #include <cassert>
-    #define PT_CORE_ASSERT(x, ...) if(!(x)) { PT_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); assert(false); }
-    #define PT_ASSERT(x, ...) if(!(x)) { PT_ERROR("Assertion Failed: {0}", __VA_ARGS__); assert(false); }
+    #define PT_CORE_ASSERT(x, ...) if(!(x)) { PT_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); DEBUG_BREAK(); }
+    #define PT_ASSERT(x, ...) if(!(x)) { PT_ERROR("Assertion Failed: {0}", __VA_ARGS__); DEBUG_BREAK(); }
 #else
     #define PT_CORE_ASSERT(x, ...)
     #define PT_ASSERT(x, ...)
 #endif
+
+#define PT_CORE_VERIFY(x, ...) if(!(x)) { PT_CORE_ERROR("Verify Failed: {0}", __VA_ARGS__); DEBUG_BREAK(); }
+#define PT_VERIFY(x, ...) if(!(x)) { PT_ERROR("Verify Failed: {0}", __VA_ARGS__); DEBUG_BREAK(); }
